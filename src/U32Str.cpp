6ccff47d22e926c9a8291f9d347b826b5cf11be4
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2024-08-17 03:50:41 +0800

    clean code

--- src/U32Str.cpp
+++ src/U32Str.cpp
@@ -4,6 +4,8 @@ namespace Pinyin
 {
     std::u32string utf8strToU32str(const std::string &utf8str) {
         std::u32string u32str;
+        u32str.reserve(utf8str.size());
+
         size_t i = 0;
         while (i < utf8str.size()) {
             char32_t ch = 0;
@@ -41,21 +43,23 @@ namespace Pinyin
 
     std::string u32strToUtf8str(const std::u32string &u32str) {
         std::string utf8str;
-        for (char32_t ch : u32str) {
+        utf8str.reserve(u32str.size() * 4);
+
+        for (const char32_t ch : u32str) {
             if (ch <= 0x7F) {
                 utf8str.push_back(static_cast<char>(ch));
             } else if (ch <= 0x7FF) {
-                utf8str.push_back(static_cast<char>(0xC0 | (ch >> 6)));
-                utf8str.push_back(static_cast<char>(0x80 | (ch & 0x3F)));
+                utf8str.push_back(static_cast<char>(0xC0 | ch >> 6));
+                utf8str.push_back(static_cast<char>(0x80 | ch & 0x3F));
             } else if (ch <= 0xFFFF) {
-                utf8str.push_back(static_cast<char>(0xE0 | (ch >> 12)));
-                utf8str.push_back(static_cast<char>(0x80 | ((ch >> 6) & 0x3F)));
-                utf8str.push_back(static_cast<char>(0x80 | (ch & 0x3F)));
+                utf8str.push_back(static_cast<char>(0xE0 | ch >> 12));
+                utf8str.push_back(static_cast<char>(0x80 | ch >> 6 & 0x3F));
+                utf8str.push_back(static_cast<char>(0x80 | ch & 0x3F));
             } else if (ch <= 0x10FFFF) {
-                utf8str.push_back(static_cast<char>(0xF0 | (ch >> 18)));
-                utf8str.push_back(static_cast<char>(0x80 | ((ch >> 12) & 0x3F)));
-                utf8str.push_back(static_cast<char>(0x80 | ((ch >> 6) & 0x3F)));
-                utf8str.push_back(static_cast<char>(0x80 | (ch & 0x3F)));
+                utf8str.push_back(static_cast<char>(0xF0 | ch >> 18));
+                utf8str.push_back(static_cast<char>(0x80 | ch >> 12 & 0x3F));
+                utf8str.push_back(static_cast<char>(0x80 | ch >> 6 & 0x3F));
+                utf8str.push_back(static_cast<char>(0x80 | ch & 0x3F));
             }
         }
         return utf8str;
